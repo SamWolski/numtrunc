f67129720a3a15842411a13d8c65be943157607f
SamWolski <wlssam007@myuct.ac.za>
2020-02-29 16:58:56 +0900

    Add TruncNum class
    The TruncNum class is the 'number' introduced by the module.

--- src/TruncNum.py
+++ src/TruncNum.py
@@ -1,11 +1,18 @@
 import numpy as np
 
+## Technical definitions used:
+## - exponent : refers to the number after the 'e' in scientific notation
+## - characteristic : the whole-number part of a decimal, ie 7 in 7.85
+## - mantissa : the non-whole-number part of a decimal, ie 0.85 in 7.85 (in NumTrunc, only the actual significant figures are considered, ie 85)
+## - decimal : <characteristic>.<mantissa>
+## - value : the actual numerical value of the stored number, ie <decimal>e<exponent>
+
 class TruncNum:
     '''
     A number that maintains its truncation, ie. the number of decimal figures, or the size of the exponent.
     '''
-    def __init__(self, value=None, model_value=None):
-        self.set_template_from_model(model_value)
+    def __init__(self, value, template=None):
+        self.template = template
         self.value = value
 
     ## Printing dunders
@@ -15,7 +22,10 @@ class TruncNum:
         return '<{} with value {} and template {}>'.format(type(self).__name__, self.value, self.template)
 
     def __str__(self):
-        return str(self.value)
+        ## Account for self._significant_figures not being set
+        sf_str = lambda s: '' if s is None else str(s)
+        format_string = '{:.'+sf_str(self._significant_figures)+'f}'
+        return format_string.format(self._decimal)+"e"+str(self.exponent)
     
     ## Arithmetic dunders
     #####################
@@ -25,10 +35,39 @@ class TruncNum:
     ## Attribute getters and setters
     ################################
 
-    ## TODO
+    @property
+    def exponent(self):
+        return self._exponent
+    ## TODO - setter
 
-    ## Logic methods
-    ################
 
-    def set_template_from_model(self, model_value):
-        self.template = None
+    @property
+    def template(self):
+        return self._template
+    @template.setter
+    def template(self, model_string):
+        self._template = True
+        ## Split the model_string into decimal and exponent
+        decimal, exponent = model_string.split("e")
+        self._exponent = int(exponent)
+        ## Check if formatting should use underscores
+        if "_" in decimal:
+            self._underscore = True
+        else:
+            self._underscore = False
+        ## Convert the mantissa into a string
+        mantissa = decimal.split(".")[1]
+        ## Remove underscores from the string and store the number of sf in mantissa
+        self._significant_figures = len(''.join(mantissa.split("_")))
+
+
+    @property
+    def value(self):
+        return self._decimal * 10**(self._exponent)
+    @value.setter
+    def value(self, value):
+        ## Adjust value such that it is in the range of the exponent
+        decimal = value * 10**(-self._exponent)
+        ## Truncate to number of significant figures after decimal point
+        self._decimal = np.round(float(decimal), self._significant_figures)
+

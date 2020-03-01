6820f26c847e2495b577fb95295017acf50971f5
SamWolski <wlssam007@myuct.ac.za>
2020-03-01 08:50:10 -0700

    Expand tests
    Now includes testing for negative values, as well as having pruned some unnecessary tests.

--- test/test_TruncNum.py
+++ test/test_TruncNum.py
@@ -26,10 +26,20 @@ models_results_float_decimal = [
         ("1.0", "3.8"),           # Fewer sf (1)
         ("1.", "4"),              # Characteristic only - w/ decimal point
         ("1", "4"),               # Characteristic only - w/o decimal point
-        ("1.00e0", "3.85e0"),     # Zero exponent
-        ("1.00e-2", "384.90e-2"), # Negative exponent
     ]
 collection_float_decimal = InputModelResultCollection(input_value_float_decimal, models_results_float_decimal)
+
+## Negative value with decimals only - no 'e'
+input_value_float_negative_decimal = -3.849
+models_results_float_negative_decimal = [
+        ("1.000", "-3.849"),       # Identity
+        ("1.00000", "-3.84900"),   # More sf
+        ("1.00", "-3.85"),         # Fewer sf (2)
+        ("1.0", "-3.8"),           # Fewer sf (1)
+        ("1.", "-4"),              # Characteristic only - w/ decimal point
+        ("1", "-4"),               # Characteristic only - w/o decimal point
+    ]
+collection_float_negative_decimal = InputModelResultCollection(input_value_float_negative_decimal, models_results_float_negative_decimal)
 
 ## Negative exponent
 input_value_float_negative_exponent = 7.86e-3
@@ -43,6 +53,7 @@ models_results_float_negative_exponent = [
         ("1.0e-3", "7.9e-3"),     # Fewer sf
         ("1.e-3", "8e-3"),        # Characteristic only - w/ decimal point
         ("1e-3", "8e-3"),         # Characteristic only - w/o decimal point
+        ("1.00e-03", "7.86e-3"),  # Padded exponent
     ]
 collection_float_negative_exponent = InputModelResultCollection(input_value_float_negative_exponent, models_results_float_negative_exponent)
 
@@ -57,15 +68,35 @@ models_results_float_positive_exponent = [
         ("1.0000e4", "1.9230e4"),  # Additional sf
         ("1.0e4", "1.9e4"),        # Fewer sf
         ("1.e4", "2e4"),           # Characteristic only - w/ decimal point
-        ("1e4", "2e4"),           # Characteristic only - w/o decimal point
+        ("1e4", "2e4"),            # Characteristic only - w/o decimal point
+        ("1.000e04", "1.923e4"),   # Padded exponent
     ]
 collection_float_positive_exponent = InputModelResultCollection(input_value_float_positive_exponent, models_results_float_positive_exponent)
+
+## Positive exponent
+input_value_float_negative_positive_exponent = -1.923e4
+models_results_float_negative_positive_exponent = [
+        ("1.000e4", "-1.923e4"),    # Identity
+        ("1.000e2", "-192.300e2"),  # Smaller exponent
+        ("1.000e5", "-0.192e5"),    # Larger exponent
+        ("1.000e7", "-0.002e7"),    # Much larger exponent
+        ("1.000e0", "-19230.000e0"),# Zero exponent
+        ("1.0000e4", "-1.9230e4"),  # Additional sf
+        ("1.0e4", "-1.9e4"),        # Fewer sf
+        ("1.e4", "-2e4"),           # Characteristic only - w/ decimal point
+        ("1e4", "-2e4"),            # Characteristic only - w/o decimal point
+        ("1.000e04", "-1.923e4"),   # Padded exponent
+    ]
+collection_float_negative_positive_exponent = InputModelResultCollection(input_value_float_negative_positive_exponent, models_results_float_negative_positive_exponent)
+
 
 ## Concatenate all lists
 input_float_with_model_params = \
     collection_float_decimal.input_models_results_list \
+    + collection_float_negative_decimal.input_models_results_list \
     + collection_float_negative_exponent.input_models_results_list \
-    + collection_float_positive_exponent.input_models_results_list
+    + collection_float_positive_exponent.input_models_results_list \
+    + collection_float_negative_positive_exponent.input_models_results_list
 
 
 ## Parametrized testing
